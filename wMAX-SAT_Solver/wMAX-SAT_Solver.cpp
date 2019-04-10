89801201e545adb6fed0531ba2bfaa28808450a3
Raphael <raphaelgoms@gmail.com>
2019-04-09 22:08:41 -0300

    Adicionar arquivos de projeto.

---
+++ wMAX-SAT_Solver/wMAX-SAT_Solver.cpp
@@ -0,0 +1,9 @@
+// wMAX-SAT_Solver.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
